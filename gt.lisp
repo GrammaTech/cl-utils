4e4d780cb47f2a541465eb5d87b15b4fa12b2fdb
Eric Schulte <eschulte@grammatech.com>
2020-02-12 20:34:16 -0500

    Add infinity

--- gt.lisp
+++ gt.lisp
@@ -59,6 +59,44 @@ An extension of `serapeum:mapconct' to include fset collections.")
           (unless (= (prog1 i (incf i)) ult)
             (write-string separator stream)))))))
 
+(defvar negative-infinity
+  #+sbcl
+  sb-ext:double-float-negative-infinity
+  #+ccl
+  ccl::double-float-negative-infinity
+  #+allegro
+  excl:*infinity-double*
+  #+ecl
+  ext:long-float-negative-infinity
+  #-(or ecl sbcl ccl allegro)
+  (error "")
+  "Most negative number.")
+
+(defvar positive-infinity
+  #+sbcl
+  sb-ext:double-float-positive-infinity
+  #+ccl
+  ccl::double-float-positive-infinity
+  #+allegro
+  excl:*infinity-double*
+  #+ecl
+  ext:long-float-positive-infinity
+  #-(or ecl sbcl ccl allegro)
+  (error "")
+  "Most positive number.")
+
+(defvar infinity positive-infinity
+  "Most positive number.")
+
+(defmethod print-object ((obj (eql negative-infinity)) stream)
+  (if *print-readably* (call-next-method) (format stream "negative-infinity")))
+
+(defmethod print-object ((obj (eql positive-infinity)) stream)
+  (if *print-readably* (call-next-method) (format stream "positive-infinity")))
+
+(defmethod print-object ((obj (eql infinity)) stream)
+  (if *print-readably* (call-next-method) (format stream "positive-infinity")))
+
 ;;; NOTE: *Consider* including Generic-cl less its new seq. stuff.
 ;;; TODO: Filesystem from UIOP.
 ;;; TODO: Process management from UIOP.
